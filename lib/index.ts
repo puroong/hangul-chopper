57a61f94d9d5d86ded2f0194bc9e9dca1472e52c
bumsoo <skfk0135@gmail.com>
2021-06-01 23:51:49 +0900

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+const hello = 'this is hello';
+
+function oneline() {
+  console.log('oneline function');
+}
+function multiline() {
+  console.log('multiline function');
+}
---
+++ test/app.spec.ts
@@ -0,0 +1,5 @@
+describe('sum test', () => {
+  it('1+1 = 2', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
